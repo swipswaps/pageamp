071f8d060c656108277e4480aabb424baac3b90f
bilbo <bilbo@ubimate.com>
2018-12-26 14:12:55 +0100

    5c1d3e623875d569be112335 pageamp/Server

--- src/pageamp/server/SrcParser.hx
+++ src/pageamp/server/SrcParser.hx
@@ -22,9 +22,15 @@
 
 package pageamp.server;
 
+import htmlparser.HtmlParser;
+import htmlparser.HtmlParser;
+import pageamp.server.SrcParser;
 import htmlparser.*;
 
 class SrcParser extends HtmlParser {
+	public static inline var NODE_POS = 0;
+	public static inline var NAME_POS = 1;
+	public static inline var VALUE_POS = 2;
 	static var patched = false;
 	public var pathname:String;
 
@@ -35,26 +41,26 @@ class SrcParser extends HtmlParser {
 
 		HtmlParser.reCDATA = "[<]!\\[CDATA\\[[\\s\\S]*?\\]\\][>]";
 		HtmlParser.reScript = "[<]\\s*script\\s*([^>]*)>([\\s\\S]*?)"
-		+ "<\\s*/\\s*script\\s*>";
+				+ "<\\s*/\\s*script\\s*>";
 		HtmlParser.reStyle = "<\\s*style\\s*([^>]*)>([\\s\\S]*?)"
-		+ "<\\s*/\\s*style\\s*>";
+				+ "<\\s*/\\s*style\\s*>";
 		HtmlParser.reElementOpen = "<\\s*(" + HtmlParser.reNamespacedID + ")";
 		HtmlParser.reAttr = HtmlParser.reNamespacedID
-		+ "(?:\\s*=\\s*(?:'[^']*?'|\"[^\"]*?\"|[-_a-z0-9]+))?";
+				+ "(?:\\s*=\\s*(?:'[^']*?'|\"[^\"]*?\"|[-_a-z0-9]+))?";
 		HtmlParser.reElementEnd = "(/)?\\s*>";
 		HtmlParser.reElementClose = "<\\s*/\\s*("
-		+ HtmlParser.reNamespacedID + ")\\s*>";
+				+ HtmlParser.reNamespacedID + ")\\s*>";
 		HtmlParser.reComment = "<!--[\\s\\S]*?-->";
 
 		HtmlParser.reMain = new EReg("(" + HtmlParser.reCDATA + ")|("
-		+ HtmlParser.reScript + ")|(" + HtmlParser.reStyle + ")|("
-		+ HtmlParser.reElementOpen + "((?:\\s+"
-		+ HtmlParser.reAttr +")*)\\s*" + HtmlParser.reElementEnd
-		+ ")|(" + HtmlParser.reElementClose + ")|("
-		+ HtmlParser.reComment + ")", "ig");
+				+ HtmlParser.reScript + ")|(" + HtmlParser.reStyle + ")|("
+				+ HtmlParser.reElementOpen + "((?:\\s+"
+				+ HtmlParser.reAttr +")*)\\s*" + HtmlParser.reElementEnd
+				+ ")|(" + HtmlParser.reElementClose + ")|("
+				+ HtmlParser.reComment + ")", "ig");
 
 		HtmlParser.reParseAttrs = new EReg("(" + HtmlParser.reNamespacedID
-		+ ")(?:\\s*=\\s*('[^']*'|\"[^\"]*\"|[-_a-z0-9]+))?" , "ig");
+				+ ")(?:\\s*=\\s*('[^']*'|\"[^\"]*\"|[-_a-z0-9]+))?" , "ig");
 
 		patched = true;
 	}
@@ -69,7 +75,7 @@ class SrcParser extends HtmlParser {
 		super();
 	}
 
-	public function getPos(n:Dynamic): SrcPos {
+	public function getPos(n:Dynamic, type=NODE_POS): SrcPos {
 		var ret:SrcPos = null;
 		if (Std.is(n, SrcElement)) {
 			ret = cast getPosition(n.i);
@@ -78,7 +84,7 @@ class SrcParser extends HtmlParser {
 			var m1 = i > 0 ? matches[i - 1] : null;
 			var p1 = i > 0 ? getPosition(i - 1) : {line:1, length:0, column:1};
 			var m2 = matches[i];
-			var p2 = cast getPosition(i);
+			var p2 = getPosition(i);
 			var line = p1.line;
 			var column = p1.column;
 			var j = 0;
@@ -87,28 +93,31 @@ class SrcParser extends HtmlParser {
 						? m1.all.substring(j, j + 2)
 						: m1.all.charAt(j);
 				if (chars == "\r\n") {
-					j += 2;
-					line++;
-					column = 1;
+					j += 2; line++; column = 1;
 				} else if (chars.charAt(0) == "\n" || chars.charAt(0) == "\r") {
-					j++;
-					line++;
-					column = 1;
+					j++; line++; column = 1;
 				} else {
-					j++;
-					column++;
+					j++; column++;
 				}
 			}
 			ret = {
 				line: line,
 				column: column,
 				length: m2.allPos - (m1 != null ? m1.allPos + m1.all.length: 0)
-			}
+			};
 		} else if (Std.is(n, SrcAttribute)) {
-			//TODO
+			var a:SrcAttribute = cast n;
+			var m = matches[a.e.i];
+			var i0 = m.allPos;
+			var re = new EReg(HtmlParser.reElementOpen, 'ig');
+			if (re.matchSub(str, m.allPos)) {
+				i0 += re.matched(0).length;
+			}
+			ret = updatePos(i0 + a.p.pos);
 		} else {
 			ret = {line:1, length:0, column:1};
 		}
+		ret != null ? ret.pathname = pathname : null;
 		return ret;
 	}
 
@@ -136,11 +145,11 @@ class SrcParser extends HtmlParser {
 					nodes:nodes, closeTagLC:ee.closeTagLC
 				};
 			} else if (m.script != null && m.script != "") {
-				var scriptNode = newElement("script", _parseAttrs(m.scriptAttrs));
+				var scriptNode = newElement("script", procAttrs(m.scriptAttrs));
 				scriptNode.addChild(new SrcText(m.scriptText, i));
 				nodes.push(scriptNode);
 			} else if (m.style != null && m.style != "") {
-				var styleNode = newElement("style", _parseAttrs(m.styleAttrs));
+				var styleNode = newElement("style", procAttrs(m.styleAttrs));
 				styleNode.addChild(new SrcText(m.styleText, i));
 				nodes.push(styleNode);
 			} else if (m.close != null && m.close != "") {
@@ -156,7 +165,8 @@ class SrcParser extends HtmlParser {
 			} else if (m.comment != null && m.comment != "") {
 				nodes.push(new SrcText(m.comment, i));
 			} else {
-				throw new HtmlParserException("Unexpected XML node.", getPosition(i));
+				throw new HtmlParserException("Unexpected XML node.",
+											  getPosition(i));
 			}
 			if (tolerant && i >= matches.length) break;
 			var curEnd = matches[i].allPos + matches[i].all.length;
@@ -164,19 +174,54 @@ class SrcParser extends HtmlParser {
 					? matches[i + 1].allPos
 					: str.length;
 			if (curEnd < nextStart) {
-				nodes.push(new SrcText(str.substr(curEnd, nextStart - curEnd), i));
+				nodes.push(new SrcText(str.substr(curEnd, nextStart - curEnd),
+									   i));
 			}
 			i++;
 		}
 		return { nodes:nodes, closeTagLC:"" };
 	}
 
-	override function newElement(name:String,
-	                             attributes:Array<HtmlAttribute>): HtmlNodeElement {
-		return new SrcElement(name, attributes, i);
+	override function parseElement(openedTagsLC:Array<String>): {
+		element:HtmlNodeElement, closeTagLC:String
+	} {
+		var tag = matches[i].tagOpen;
+		var tagLC = matches[i].tagOpenLC;
+		var attrs = matches[i].attrs;
+		var isWithClose = matches[i].tagEnd != null
+				&& matches[i].tagEnd != ""
+				|| isSelfClosingTag(tagLC);
+		var elem = newElement(tag, procAttrs(attrs));
+		var closeTagLC = "";
+		if (!isWithClose) {
+			i++;
+			openedTagsLC.push(tagLC);
+			var m = processMatches(openedTagsLC);
+			for (node in m.nodes) elem.addChild(node);
+			openedTagsLC.pop();
+			closeTagLC = m.closeTagLC != tagLC ? m.closeTagLC : "";
+			if (i < matches.length || !tolerant) {
+				if (matches[i].close == null
+						|| matches[i].close == ""
+						|| matches[i].tagCloseLC != tagLC) {
+					if (!tolerant) {
+						throw new HtmlParserException("Tag <" + tag
+								+ "> not closed.", getPosition(i));
+					} else {
+						closeTagLC = matches[i].tagCloseLC;
+					}
+				}
+			}
+		}
+		return { element:elem, closeTagLC:closeTagLC };
 	}
 
-	function _parseAttrs(str:String): Array<HtmlAttribute> {
+	override function newElement(name:String,
+	                             attrs:Array<HtmlAttribute>): HtmlNodeElement {
+		return new SrcElement(name, attrs, i);
+	}
+
+	function procAttrs(str:String): Array<HtmlAttribute> {
 		var attributes = new Array<HtmlAttribute>();
 		var pos = 0;
 		while (pos < str.length && HtmlParser.reParseAttrs.matchSub(str, pos)) {
@@ -193,17 +238,38 @@ class SrcParser extends HtmlParser {
 				}
 				unescaped = HtmlTools.unescape(value);
 			}
-			attributes.push(new HtmlAttribute(name, unescaped, quote));
 			var p = HtmlParser.reParseAttrs.matchedPos();
+			attributes.push(new SrcAttribute(name, unescaped, quote, p));
 			pos = p.pos + p.len;
 		}
 		return attributes;
+	}
+
+	function updatePos(i:Int, i0=0, ?p:SrcPos): SrcPos {
+		p == null ? p = {line:1, column:1, length:0, pathname:pathname} : null;
+		while (i0 < i) {
+			var chars = i0 + 1 < str.length
+					? str.substring(i0, i0 + 2)
+					: str.charAt(i0);
+			if (chars == '\r\n') {
+				i0 += 2; p.line++; p.column = 1;
+			} else if (chars.charAt(0) == '\n' || chars.charAt(0) == '\r') {
+				i0++; p.line++; p.column = 1;
+			} else {
+				i0++; p.column++;
+			}
+		}
+		return p;
 	}
 
 }
 
 typedef SrcPos = {
 	line: Int, length: Int, column: Int, ?pathname: String,
+}
+
+typedef AttrPos = {
+	pos: Int, len: Int,
 }
 
 @:access(htmlparser.HtmlParser)
@@ -219,8 +285,8 @@ class SrcDocument extends HtmlDocument {
 		}
 	}
 
-	public function getPos(n:Dynamic): SrcPos {
-		return (parser != null ? parser.getPos(n) : null);
+	public function getPos(n:Dynamic, type=SrcParser.NODE_POS): SrcPos {
+		return (parser != null ? parser.getPos(n, type) : null);
 	}
 
 }
@@ -228,9 +294,14 @@ class SrcDocument extends HtmlDocument {
 class SrcElement extends HtmlNodeElement {
 	public var i(default,null): Int;
 
+	@:access(pageamp.server.SrcAttribute)
 	public function new(name:String, attributes:Array<HtmlAttribute>, i:Int) {
 		this.i = i;
 		super(name, attributes);
+		var srcAtts:Array<SrcAttribute> = cast attributes;
+		for (a in srcAtts) {
+			a.e = this;
+		}
 	}
 
 }
@@ -246,10 +317,11 @@ class SrcText extends HtmlNodeText {
 }
 
 class SrcAttribute extends HtmlAttribute {
-	public var i(default,null): Int;
+	public var e(default,null): SrcElement;
+	public var p(default,null): AttrPos;
 
-	public function new(name:String, value:String, quote:String, i:Int) {
-		this.i = i;
+	public function new(name:String, value:String, quote:String, p:AttrPos) {
+		this.p = p;
 		super(name, value, quote);
 	}
 
--- test/pageamp/server/SrcParserTest.hx
+++ test/pageamp/server/SrcParserTest.hx
@@ -1,5 +1,6 @@
 package pageamp.server;
 
+import htmlparser.HtmlParser;
 import pageamp.server.SrcParser;
 import haxe.unit.TestCase;
 
@@ -51,6 +52,48 @@ class SrcParserTest extends TestCase {
 		assertEquals(2, pos.line);
 		assertEquals(2, pos.column);
 		assertEquals(14, pos.length);
+	}
+
+	function testNodePos3() {
+		var parser = new SrcParser();
+		var doc:SrcDocument = SrcParser.parseDoc(''
+		+ '<root a="v1"\n'
+		+ '      b="v2"/>');
+		var root = doc.children[0];
+		var a1 = root.attributes[0];
+		assertTrue(Std.is(a1, SrcAttribute));
+		assertEquals(a1.name, 'a');
+		var p1 = doc.getPos(a1);
+		assertEquals(1, p1.line);
+		assertEquals(7, p1.column);
+		var a2 = root.attributes[1];
+		assertTrue(Std.is(a2, SrcAttribute));
+		assertEquals(a2.name, 'b');
+		var p2 = doc.getPos(a2);
+		assertEquals(2, p2.line);
+		assertEquals(7, p2.column);
+	}
+
+	function testNodePos4() {
+		var parser = new SrcParser();
+		var doc:SrcDocument = SrcParser.parseDoc('<root>\n'
+		+ '<element a="v1"\n'
+		+ '    b="v2"/>\n'
+		+ '</root>');
+		var root = doc.children[0];
+		var element = root.children[0];
+		var a1 = element.attributes[0];
+		assertTrue(Std.is(a1, SrcAttribute));
+		assertEquals(a1.name, 'a');
+		var p1 = doc.getPos(a1);
+		assertEquals(2, p1.line);
+		assertEquals(10, p1.column);
+		var a2 = element.attributes[1];
+		assertTrue(Std.is(a2, SrcAttribute));
+		assertEquals(a2.name, 'b');
+		var p2 = doc.getPos(a2);
+		assertEquals(3, p2.line);
+		assertEquals(5, p2.column);
 	}
 
 }
